cd9d486f2ab5b1ebf582a2fbb705946a73832986
seanshnkim <sehyun.seankim@gmail.com>
2025-02-16 16:33:17 +0000

    Deploying to gh-pages from @ seanshnkim/seanshnkim.github.io@b5654feb99094c40bd58025b4bb275593f20a247 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,15 +23,48 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "post-semaphore-vs-mutex",
+        },{id: "post-sort-in-python-and-c",
       
-        title: "Semaphore vs. mutex",
+        title: "Sort in Python and C++",
       
       description: "",
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/blog/2025/Semaphore-vs.-Mutex/";
+          window.location.href = "/blog/2025/Sort-in-Python-and-CPP/";
+        
+      },
+    },{id: "post-two-pointers",
+      
+        title: "Two Pointers",
+      
+      description: "Different approaches to solve two pointer algorithm problems",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/Two-Pointers/";
+        
+      },
+    },{id: "post-schedulers",
+      
+        title: "Schedulers",
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/Schedulers/";
+        
+      },
+    },{id: "post-semaphore-vs-mutex",
+      
+        title: "Semaphore vs. Mutex",
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/Semaphore-versus-Mutex/";
         
       },
     },{id: "post-solutions-for-priority-inversion",
@@ -54,6 +87,138 @@ ninja.data = [{
       handler: () => {
         
           window.location.href = "/blog/2025/Illustrated-Priority-Inversion/";
+        
+      },
+    },{id: "post-analysis-on-39-third-maximum-number-39-problem-in-depth",
+      
+        title: "Analysis on &#39;Third Maximum Number&#39; Problem in Depth",
+      
+      description: "How to improve code efficiency",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/Third-Maximum-Number/";
+        
+      },
+    },{id: "post-mistake-collections",
+      
+        title: "Mistake Collections",
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/Mistakes/";
+        
+      },
+    },{id: "post-array-and-hashing-part-1",
+      
+        title: "Array and Hashing Part 1",
+      
+      description: "How to handle array and hash map &amp; set",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/Array-and-Hashing/";
+        
+      },
+    },{id: "post-queue-priority-queue-and-vector-in-c",
+      
+        title: "Queue, Priority Queue and Vector in C++",
+      
+      description: "Convenient Cheat Sheet for queue, priority queue and vector methods in vector",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/Queue,-Priority-Queue-and-Vector-in-C++/";
+        
+      },
+    },{id: "post-heap-and-priority-queue-problems",
+      
+        title: "Heap and Priority Queue Problems",
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/Heap-and-Priority-Queue-Problems/";
+        
+      },
+    },{id: "post-bit-manipulation",
+      
+        title: "Bit Manipulation",
+      
+      description: "Bit manipulations and useful techniques",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/Bit-Manipulation/";
+        
+      },
+    },{id: "post-tree-and-graph",
+      
+        title: "Tree and Graph",
+      
+      description: "Basic concepts on tree and graph",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/tree/";
+        
+      },
+    },{id: "post-bfs-breadth-first-search-and-queue",
+      
+        title: "BFS (Breadth First Search) and Queue",
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/bfs-and-queue/";
+        
+      },
+    },{id: "post-dfs-depth-first-search-and-recursion",
+      
+        title: "DFS (Depth First Search) and Recursion",
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/dfs-and-recursion/";
+        
+      },
+    },{id: "post-list-and-vector-in-python-and-in-c",
+      
+        title: "List and vector in Python and in C++",
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/list-and-vector-in-python-and-in-cpp/";
+        
+      },
+    },{id: "post-hash-map-and-set-in-python-and-in-c",
+      
+        title: "Hash map and set in Python and in C++",
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/hash-map-and-set-in-python-and-in-cpp/";
+        
+      },
+    },{id: "post-priority-queue-in-python-and-in-cpp",
+      
+        title: "Priority Queue in Python and in CPP",
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2024/priority-queue-in-python-and-in-cpp/";
         
       },
     },{id: "news-began-the-first-semester-at-nyu-tandon-school-of-engineering",
